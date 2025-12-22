2525db6b1b3f9cfe0840afb4334820328896e85e
Ivan <ivan.taim@jandex.ru>
2025-12-22 19:19:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*007 простое общение с пользователем
+// alert('Hello!')
+
+/!*const result = confirm('Are you here?')
+console.log(result)*!/
+/!*
+const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+
+document.write(answers);*!/*/
+
+/*// 008 интерполяция и бектики
+
+const user = 'Иван';
+
+alert(`Привет, ${user}!`);*/
+
