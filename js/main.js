43196b1edf619a331bcd447ae126cfbcc7810673
ivan-tv <ivan.taim@jandex.re>
2025-12-23 22:31:41 +0200

    went 014

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+/*
+
+// 013 if... else... тернарный оператор
+// const num = 5;
+//
+// if (num === 4) {
+//   console.log(num)
+// } else if (num > 5){
+//   console.log("многовато будет!")
+// } else {
+//   console.log('маловато будет')
+// }
+//
+// (num === 5) ? console.log('всё верно') : console.log('ты ошибся');
+
+     // 014 ЦИКЛЫ
+
+// let num = 50;
+
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
+// }
+
+//  do {
+//   console.log(num);
+//   num++;
+// }
+// while (num <= 55);
+ 
+//  for (let i = 1; i < 8; i++) {
+//    console.log(num);
+//    num++;
+// }
+
+ // for (let i = 1; i < 10; i++) {
+ //   if (i === 6) {
+ //    continue;
+     // break;
+   // }
+   // console.log(i);
+ // }
+*/
