cdd6424c5fb907dfe179625d61c55c12cb1ecaf2
ivan-tv <ivan.taim@jandex.re>
2025-12-24 22:32:06 +0200

    made the second part of the application

--- js/script.js
+++ js/script.js
@@ -3,19 +3,34 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+  count: numberOfFilms, movies: {}, actors: {}, genres: [], privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = +prompt('На сколько оцените его?', ''),
- c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
 
 console.log(personalMovieDB);
+
+
+
+
